85f3d95612c418acf107c7efa2c3b55eb60a7a29
Julian Paulozzi <julianpaulozzi@gmail.com>
2014-11-24 14:51:23 -0200

    Commit client test src

---
+++ src/MetroShell.ClientTest/MetroShell.ClientTest/Client/ThemeProvider.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace LightSwitchApplication
+{
+    public class ShellProvider : ExtensionsMadeEasy.ClientAPI.Shell.EasyShellExporter<PaulozziCo.MetroShell.Presentation.Shells.Components.MetroShell> { }
+    public class ThemeProvider : ExtensionsMadeEasy.ClientAPI.Theme.EasyThemeWithExtensionsExporter<PaulozziCo.MetroShell.Presentation.Themes.MetroTheme, PaulozziCo.MetroShell.Presentation.Themes.MetroStyles> { }
+}
